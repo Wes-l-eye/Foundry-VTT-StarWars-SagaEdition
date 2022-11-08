29a54ec316b2b6697c1801bb7031a8eccb25c302
kypvalanx <lijewskirocks@gmail.com>
2022-11-08 07:43:59 -0600

    * generated standard infrastructure including jest impl

---
+++ src/module/actor/species.js
@@ -0,0 +1,10 @@
+/**
+ *
+ * @param actor {SWSEActor}
+ */
+export function generateSpeciesData(actor) {
+  if (!actor.species) {
+    return;
+  }
+  actor.system.attributes.con.skip = actor.isDroid;
+}
